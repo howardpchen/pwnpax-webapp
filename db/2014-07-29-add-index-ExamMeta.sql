f590fe3862de32e618ac38a7210a27551a22ede5
Howard <howardpchen.mail@gmail.com>
2020-03-02 22:00:33 +0000

    Remove db directory which is not needed anymore

--- db/2014-07-29-add-index-ExamMeta.sql
+++
@@ -1,8 +0,0 @@
--- 2014-07-29-add-index-ExamMeta.sql
--- This is a database migration that updates an existing database
--- so that the table names match the codebase.
--- this is important for linux systems
-
-use `capricorn`;
-
-alter table ExamMeta add index (`AccessionNumber`);
